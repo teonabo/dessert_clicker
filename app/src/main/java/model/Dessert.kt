4fbd7564636711a67c2428afc688b0182fde8a61
Teona <bojoska.teona@uklo.edu.mk>
2025-03-14 14:25:12 +0100

    Domashna zadaca 3

---
+++ app/src/main/java/model/Dessert.kt
@@ -0,0 +1,5 @@
+
+/**
+ * [Dessert] is the data class to represent the Dessert imageId, price, and startProductionAmount
+ */
+data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
